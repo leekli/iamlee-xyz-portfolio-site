a90b4dc11dd17f680670574947b4aad465ae87b5
leekli <li.kirkham@live.com>
2023-01-22 20:32:06 +0000

    upload 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <h1>iamlee.xyz</h1>
+      <a
+        href="https://www.linkedin.com/in/leekirkham/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src="assets/linkedin.png"
+          alt="This goes to my LinkedIn page"
+          width="25"
+        />
+      </a>
+      &nbsp;
+      <a href="https://twitter.com/likirkham" target="_blank" rel="noreferrer">
+        <img
+          src="assets/twitter.png"
+          alt="This goes to my Twitter page"
+          width="25"
+        />
+      </a>
+      &nbsp;
+      <a
+        href="https://www.reddit.com/user/lee_xyz"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src="assets/reddit.jpeg"
+          alt="This goes to my Reddit page"
+          width="25"
+        />
+      </a>
+    </main>
   );
 }
 
